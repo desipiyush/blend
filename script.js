804745c13f1f66207bc5af25ab328bf5c16806e7
desipiyush <89911392+desipiyush@users.noreply.github.com>
2021-10-19 20:14:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var aa = document.getElementById('aa');
+var bb = document.getElementById('bb');
+var xad = document.getElementById('xad');
+xad.play();
+function a() {
+    aa.style.backgroundColor = "green";
+    bb.style.backgroundColor = "green";
+}
+function b() {
+    aa.style.backgroundColor = "red";
+    bb.style.backgroundColor = "red";
+}
+function c() {
+    aa.style.backgroundColor = "blue";
+    bb.style.backgroundColor = "blue";
+}
